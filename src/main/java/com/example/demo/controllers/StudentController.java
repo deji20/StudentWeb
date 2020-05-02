584d8a2bd1b55ccafea4008ab38df6deea931e38
Deji <ehinlanwo.deji@live.dk>
2020-05-02 17:26:01 +0200

    tilføjede en profil billed attribut til student modellen for det tror jeg vil være sjovt at gøre lavet detail og student listen lidt om og laved et statisk hoved jeg syntes vi burde bruge.

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -48,10 +48,9 @@ public class StudentController {
     }
 
     @GetMapping("/student")
-    @ResponseBody
-    public String getStudentByParameter(@RequestParam int id) {
-        Student stu = studentRepository.read(id);
-        return "The name is: " + stu.getFirstName() + " and the cpr is " + stu.getCpr();
+    public String getStudentByParameter(Model model, @RequestParam int id) {
+        model.addAttribute("student", studentRepository.read(id));
+        return "detail";
     }
 
 
